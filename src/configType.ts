f2481138f0ffa3496efc9dbaad50c7d60a0a8627
Max von Webel <max@343max.de>
2024-02-24 16:24:12 +0100

    made the title changeable

--- src/configType.ts
+++ src/configType.ts
@@ -3,4 +3,5 @@ import { z } from "zod"
 export const configSchema = z.object({
   POCKET_CASTS_EMAIL: z.string(),
   POCKET_CASTS_PASSWORD: z.string(),
+  POCKET_CASTS_PODCAST_TITLE: z.string(),
 })
--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ const config = configSchema.parse(process.env)
 const main = async () => {
   const pocketcasts = await pocketCastsLogin(config.POCKET_CASTS_EMAIL, config.POCKET_CASTS_PASSWORD)
   const podcasts = await pocketcasts.podcastList()
-  const figarino = podcasts.podcasts.filter(({ title }) => title.includes("Figarino"))[0]
-  const bookmarks = await pocketcasts.bookmarks(figarino.uuid)
+  const podcast = podcasts.podcasts.filter(({ title }) => title.includes(config.POCKET_CASTS_PODCAST_TITLE))[0]
+  const bookmarks = await pocketcasts.bookmarks(podcast.uuid)
   const archivedEpisodes = bookmarks.episodes.filter(({ isDeleted }) => isDeleted).map(({ uuid }) => uuid)
   await pocketcasts.archive(
     false,
-    archivedEpisodes.map((uuid) => ({ uuid, podcast: figarino.uuid }))
+    archivedEpisodes.map((uuid) => ({ uuid, podcast: podcast.uuid }))
   )
-  console.log(`unarchived ${archivedEpisodes.length} episodes of '${figarino.title}'`)
+  console.log(`unarchived ${archivedEpisodes.length} episodes of '${podcast.title}'`)
 }
 
 main()
