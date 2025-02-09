61318239c508ffab0af11260f5dedeb2214b4e99
Max von Webel <max@343max.com>
2025-02-09 21:06:57 +0100

    added support for multiple podcasts

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,29 @@ const config = configSchema.parse(process.env)
 
 const main = async () => {
   const pocketcasts = await pocketCastsLogin(config.POCKET_CASTS_EMAIL, config.POCKET_CASTS_PASSWORD)
-  const podcasts = await pocketcasts.podcastList()
-  const podcast = podcasts.podcasts.filter(({ title }) => title.includes(config.POCKET_CASTS_PODCAST_TITLE))[0]
-  const bookmarks = await pocketcasts.bookmarks(podcast.uuid)
-  const archivedEpisodes = bookmarks.episodes.filter(({ isDeleted }) => isDeleted).map(({ uuid }) => uuid)
-  await pocketcasts.archive(
-    false,
-    archivedEpisodes.map((uuid) => ({ uuid, podcast: podcast.uuid }))
-  )
-  console.log(`unarchived ${archivedEpisodes.length} episodes of '${podcast.title}'`)
-  const startedEpisodes = bookmarks.episodes.filter(({ playedUpTo }) => playedUpTo > 0).map(({ uuid }) => uuid)
-  console.log(`resetting ${startedEpisodes.length} episodes of '${podcast.title}'`)
-  for (const episodeUuid of startedEpisodes) {
-    await pocketcasts.updateEpisode(episodeUuid, podcast.uuid, { position: "0", status: 2 })
-    process.stdout.write(".")
+  const podcastList = await pocketcasts.podcastList()
+  const podcastsToUnarchive = config.POCKET_CASTS_PODCAST_TITLE.split(",").map((t) => t.trim())
+  for (const podcastTitle of podcastsToUnarchive) {
+    const podcast = podcastList.podcasts.filter(({ title }) => title.includes(podcastTitle))[0]
+    const bookmarks = await pocketcasts.bookmarks(podcast.uuid)
+    const archivedEpisodes = bookmarks.episodes.filter(({ isDeleted }) => isDeleted).map(({ uuid }) => uuid)
+    await pocketcasts.archive(
+      false,
+      archivedEpisodes.map((uuid) => ({ uuid, podcast: podcast.uuid }))
+    )
+    console.log(`unarchived ${archivedEpisodes.length} episodes of '${podcast.title}'`)
+    const startedEpisodes = bookmarks.episodes.filter(({ playedUpTo }) => playedUpTo > 0).map(({ uuid }) => uuid)
+    console.log(`resetting ${startedEpisodes.length} episodes of '${podcast.title}'`)
+    for (const episodeUuid of startedEpisodes) {
+      await pocketcasts.updateEpisode(episodeUuid, podcast.uuid, {
+        position: "0",
+        status: 2,
+      })
+      process.stdout.write(".")
+    }
+    console.log()
+    console.log(`reset ${startedEpisodes.length} episodes of '${podcast.title}'`)
   }
-  console.log()
-  console.log(`reset ${startedEpisodes.length} episodes of '${podcast.title}'`)
 }
 
 main()
